8bffcfe8dbd901eb75a32f0c494aff7b2632404f
Amin Faruq <aminfaruk.fa@gmail.com>
2022-10-31 14:09:49 +0800

    build new target `Meal recipes iOS`

---
+++ Meal Recipes/Meal Recipes iOS/Meal_Recipes_iOS.h
@@ -0,0 +1,18 @@
+//
+//  Meal_Recipes_iOS.h
+//  Meal Recipes iOS
+//
+//  Created by Amin faruq on 31/10/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Meal_Recipes_iOS.
+FOUNDATION_EXPORT double Meal_Recipes_iOSVersionNumber;
+
+//! Project version string for Meal_Recipes_iOS.
+FOUNDATION_EXPORT const unsigned char Meal_Recipes_iOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Meal_Recipes_iOS/PublicHeader.h>
+
+
