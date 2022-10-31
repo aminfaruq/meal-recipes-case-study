4af04be671d11958b21b604d52e515b8fac350d0
Amin Faruq <aminfaruk.fa@gmail.com>
2022-10-31 12:39:57 +0800

    implement shared api

---
+++ Meal Recipes/Meal Recipes/Meal_Recipes.h
@@ -0,0 +1,18 @@
+//
+//  Meal_Recipes.h
+//  Meal Recipes
+//
+//  Created by Amin faruq on 31/10/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Meal_Recipes.
+FOUNDATION_EXPORT double Meal_RecipesVersionNumber;
+
+//! Project version string for Meal_Recipes.
+FOUNDATION_EXPORT const unsigned char Meal_RecipesVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Meal_Recipes/PublicHeader.h>
+
+
